da698ed4c5857f009b35d0761f6e5fc91edf31db
fahdfayed <fahd.fayedofficial@gmail.com>
2024-04-15 17:34:51 +0400

    About Us

--- src/App.js
+++ src/App.js
@@ -268,7 +268,7 @@ function App() {
           <div className="about-us-section">
   <h2 className="about-us-title">About Us</h2>
   <p className="about-us-content">
-  This is the second phase of the study. Please press on the button in order to provide access to your Spotify music data. The data and a form link will be presented. Please complete the form by copy-pasting the values. 
+  This is the second phase of the study. Please press on the button in order to provide access to your Spotify music data. The data and a form link will be presented. Please complete the form by copy-pasting the <i>values</i>values. 
   <br />
 
   <Strong> Notice: The researcher will not have access to any login information provided.</Strong>
